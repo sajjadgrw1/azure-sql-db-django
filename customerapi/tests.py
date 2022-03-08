c21a9cfe66518fccd1780ad64e73692dc8ee0383
sajjadgrw1 <sajjadgrw1@gmail.com>
2022-03-08 18:55:13 +0500

    docker compose
    latest push docker compose.

--- customerapi/tests.py
+++ customerapi/tests.py
@@ -1,20 +1,27 @@
-from lib2to3.pgen2.token import tok_name
-from urllib import request, response
+
+from email import header
+import json
+
+from django.contrib.auth.models import User
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from rest_framework.test import APIClient
+from rest_framework.authtoken.models import Token
 
+from customerapi.models import Restaurant, Menu, Vote
 
-class login(TestCase):
-    def setUp(self):
-        response = self.client.post('/api/login/', {
-            "username": "sajjad5",
-            "password": "hellosajjad"
-        })
-        self.assertEqual(response.status_code, 200)
-
-# Create your tests here.
-
-
-# class TestRestaurant(TestCase):
-#     def test_restaurant_create(self):
-#         response = self.client.get('api/restaurant')
-#         self.assertEqual(response.status_code, 200)
+class RestaurantTest(APITestCase):
+    def test_listRestaurant(self):
+        queryset = Restaurant.objects.all()
+        print(queryset)
+        # response = self.client.get('/api/restaurant' ,  headers={ 'Authorization': 'Token bb11edce6734b6139bdcf0e28f1c801b162d94e69eb4ef884eaafd9b900c785c' } )
+        # self.assertEqual(response.status_code, status.HTTP_200_OK )
+        #self.assertEqual(len(response.data), 0)
+        # client = APIClient()
+        # result = client.login(username='sajjad4', password='hellosajjad')
+        # print(result)
+        #token = Token.objects.get(user__username='sajjad4')
+        #print(token)
+        
